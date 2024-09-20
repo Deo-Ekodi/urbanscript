8cb2cf8388f25243a4ebc563814b27f6da564b5e
Deo-Ekodi <deoekodi@gmail.com>
2024-09-20 02:27:45 -0800

    switch to next.js

---
+++ urbanscript_dummy/src/index.js
@@ -0,0 +1,56 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css';
+
+// const formContainer = document.getElementById('formContainer');
+// const imageResultsContainer = document.getElementById('imageResultsContainer');
+
+// const root = ReactDOM.createRoot(formContainer);
+// root.render(<App />);
+
+// Render App only in the designated container
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css';
+
+// const root = ReactDOM.createRoot(document.getElementById('formContainer'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const formContainer = document.getElementById('formContainer');
+const root = ReactDOM.createRoot(formContainer);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
