4ad29c9e199d08adf965050ac76fc17215cbbb71
Deo-Ekodi <deoekodi@gmail.com>
2024-09-26 06:23:44 -0800

    user authentication - functional

---
+++ urbanscript/app/api/userExists/route.js
@@ -0,0 +1,36 @@
+// import { connectMongoDB } from "@/lib/mongodb";
+// import User from "@/models/User";
+// import { NextResponse } from "next/server";
+
+// export async function POST(req) {
+//   try {
+//     await connectMongoDB();
+//     const { email } = await req.json();
+//     const user = await User.findOne({ email }).select("_id");
+//     console.log("user: ", user);
+//     return NextResponse.json({ user });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/User";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    await connectMongoDB();
+    const { email } = await req.json();
+    const user = await User.findOne({ email }).select("_id");
+    console.log("user: ", user);
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.error("Error checking user existence: ", error);
+    return NextResponse.json(
+      { message: "Error checking user existence", error: error.message },
+      { status: 500 }
+    );
+  }
+}
