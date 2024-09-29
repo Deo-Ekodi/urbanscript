101061743f6cbeff48efe2d88fce0f541af5c3b3
Deo-Ekodi <deoekodi@gmail.com>
2024-09-29 04:11:47 -0800

    adjust mongodb import to relative paths

--- urbanscript/app/api/contact/route.js
+++ urbanscript/app/api/contact/route.js
@@ -33,7 +33,7 @@
 //   }
 // }
 
-import { connectMongoDB } from "@/lib/mongodb";
+import { connectMongoDB } from "../../../lib/mongodb";
 import Contact from "@/models/Contact/Contact";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
