5c6589a2c4a47a2a54f3d4463a16deb58c4d419e
Deo-Ekodi <deoekodi@gmail.com>
2024-09-29 03:50:20 -0800

    mongodb issue on deployment

--- urbanscript/app/api/userExists/route.js
+++ urbanscript/app/api/userExists/route.js
@@ -1,4 +1,4 @@
-import { connectMongoDB } from "@/lib/mongodb/mongodb";
+import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/User/User";
 import { NextResponse } from "next/server";
 
